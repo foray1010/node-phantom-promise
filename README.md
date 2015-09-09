b501c8909ee0be5b073eb5634f2e27877a0b621d
foray1010 <foray1010@users.noreply.github.com>
2015-09-09 19:22:33 +0800

    Init node-phantom-promise

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # node-phantom-promise
+
 A simple wrapper for baudehlo/node-phantom-simple with promise
---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const driver = require('node-phantom-simple')
+
+function genPromiseFunc(originalFunc) {
+  return function () {
+    const _args = Array.prototype.slice.call(arguments)
+    const _this = this
+
+    return new Promise(function (resolve, reject) {
+      _args.push(function () {
+        const _args2 = Array.prototype.slice.call(arguments)
+
+        const err = _args2.shift()
+
+        if (err) {
+          reject(err)
+        } else {
+          const _arg3 = _args2.map(function (arg) {
+            return promisifyAll(arg)
+          })
+
+          resolve.apply(null, _arg3)
+        }
+      })
+
+      originalFunc.apply(_this, _args)
+    })
+  }
+}
+
+function promisifyAll(target) {
+  if (typeof target !== 'object' || Array.isArray(target)) {
+    return target
+  }
+
+  const promisifiedTarget = {}
+
+  for (let targetPropName of Object.keys(target)) {
+    if (typeof target[targetPropName] !== 'function') {
+      promisifiedTarget[targetPropName] = target[targetPropName]
+      continue
+    }
+
+    promisifiedTarget[targetPropName] = genPromiseFunc(target[targetPropName])
+  }
+
+  return promisifiedTarget
+}
+
+return promisifyAll(driver)
