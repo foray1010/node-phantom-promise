25d0daeef6c1cb5ea1b5a6c4f9f893c289cff08f
foray1010 <foray1010@users.noreply.github.com>
2015-09-17 14:26:30 +0800

    fill onWhatever callback when it is a page object

--- index.js
+++ index.js
@@ -2,6 +2,25 @@
 
 const driver = require('node-phantom-simple')
 
+// copy from node-phantom-simple/bridge.js
+const pageCallbacks = [
+  'onAlert', 'onCallback', 'onClosing', 'onConfirm', 'onConsoleMessage', 'onError', 'onFilePicker',
+  'onInitialized', 'onLoadFinished', 'onLoadStarted', 'onNavigationRequested',
+  'onPrompt', 'onResourceRequested', 'onResourceReceived', 'onResourceTimeout', 'onResourceError', 'onUrlChanged',
+  // SlimerJS only
+  'onAuthPrompt'
+]
+// copy from node-phantom-simple/node-phantom-simple.js
+const pageMethods = [
+  'addCookie', 'childFramesCount', 'childFramesName', 'clearCookies', 'close',
+  'currentFrameName', 'deleteCookie', 'evaluateJavaScript',
+  'evaluateAsync', 'getPage', 'go', 'goBack', 'goForward', 'includeJs',
+  'injectJs', 'open', 'openUrl', 'release', 'reload', 'render', 'renderBase64',
+  'sendEvent', 'setContent', 'stop', 'switchToFocusedFrame', 'switchToFrame',
+  'switchToFrame', 'switchToChildFrame', 'switchToChildFrame', 'switchToMainFrame',
+  'switchToParentFrame', 'uploadFile', 'clearMemoryCache'
+]
+
 function genPromiseFunc(originalFunc) {
   return function () {
     const _args = Array.prototype.slice.call(arguments)
@@ -42,6 +61,26 @@ function promisifyAll(target) {
     promisifiedTarget[targetPropName] = typeof targetPropValue === 'function' ?
       genPromiseFunc(targetPropValue) :
       targetPropValue
+  }
+
+  // fill onWhatever callback when it is a page object
+  const isPage = pageMethods.every(function (pageMethodName) {
+    return Boolean(target[pageMethodName])
+  })
+  if (isPage) {
+    const propsDescriptor = {}
+    for (let pageCallbackName of pageCallbacks) {
+      propsDescriptor[pageCallbackName] = {
+        get: function () {
+          return target[pageCallbackName]
+        },
+        set: function (newValue) {
+          target[pageCallbackName] = newValue
+        }
+      }
+    }
+
+    Object.defineProperties(promisifiedTarget, propsDescriptor)
   }
 
   return promisifiedTarget
